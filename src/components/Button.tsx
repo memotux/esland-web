4f9dfa73d8789336f5d505fc71d83130ec68dd99
memoTux <romeo@mendezfuentes.net>
2024-02-25 21:13:59 -0600

    [components] Button function component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import type { SetupContext } from 'vue'
 import styles from '@/components/styles/Button.module.css'
 
 interface ButtonProps {
@@ -8,37 +8,41 @@ interface ButtonProps {
   url: string
 }
 
-export default defineComponent<ButtonProps>({
-  emits: ['click'],
-  render() {
-    const { url, target, className } = this.$props
-    return (
-      <a
-        href={url}
-        target={`${target ?? '_blank'}`}
-        rel="noopener noreferrer"
-        onClick={() => this.$emit('click')}
-        class={`
-  lg:text-2xl
-  md:px-5
-  md:text-xl
-  text-xs
-  w-fit
-  font-medium
-  no-underline
-  px-3
-    py-3
-    border
-    border-solid
-    border-white
-    rounded-full
-    uppercase
-    ${styles.button}
-    ${className ?? ''}`}
-        {...this.$attrs}
-      >
-        {this.$slots.default!()}
-      </a>
-    )
-  },
-})
+interface ButtonEmits {
+  click: (e: MouseEvent) => void | Promise<void>
+}
+
+export default function Button(
+  props: ButtonProps,
+  { emit, slots, attrs }: SetupContext<ButtonEmits>
+) {
+  const { url, target, className } = props
+  return (
+    <a
+      href={url}
+      target={`${target ?? '_blank'}`}
+      rel="noopener noreferrer"
+      onClick={(e) => emit('click', e)}
+      class={`
+          lg:text-2xl
+          md:px-5
+          md:text-xl
+          text-xs
+          w-fit
+          font-medium
+          no-underline
+          px-3
+            py-3
+            border
+            border-solid
+            border-white
+            rounded-full
+            uppercase
+            ${styles.button}
+            ${className ?? ''}`}
+      {...attrs}
+    >
+      {slots.default?.() ?? 'Button'}
+    </a>
+  )
+}
