262f21dc0250effed638684a957444cb5e6a5fe6
memoTux <romeo@mendezfuentes.net>
2024-02-25 21:13:05 -0600

    [conf] auth.config Github provider

--- auth.config.js
+++ auth.config.js
@@ -1,11 +1,11 @@
-import Twitch from '@auth/core/providers/twitch'
+import Github from '@auth/core/providers/github'
 import { defineConfig } from 'auth-astro'
 
 export default defineConfig({
-	providers: [
-		Twitch({
-			clientId: import.meta.env.TWITCH_CLIENT_ID,
-			clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
-		}),
-	],
+  providers: [
+    Github({
+      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
 })
