a62e5a410e95c05858efe26d4b40d3c520443934
memoTux <romeo@mendezfuentes.net>
2024-02-29 22:14:04 -0600

    [components] Palmares reactivity

--- src/components/Palmares.tsx
+++ src/components/Palmares.tsx
@@ -1,91 +1,96 @@
+import { computed, defineComponent, ref } from 'vue'
 import StreamerCard from '@/components/StreamerCard.tsx'
 import palmares from '@/data/editions-info.json'
 import styles from '@/components/styles/Palmares.module.css'
-import type { I18N } from '@/i18n'
 
-export default function Palmares({ i18n, edicion }: { i18n: I18N; edicion: string }) {
-  let index = 3
+export default defineComponent({
+  name: 'Palmares',
+  props: {
+    i18n: {
+      type: Object,
+      required: true,
+    },
+    edicion: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const index = ref(3)
 
-  let datosSeleccionados = palmares.find((palmar) => palmar.edition === edicion)
+    const palmar = computed(
+      () => palmares.find((palmar) => palmar.edition === props.edicion)?.info[index.value]
+    )
 
-  if (!datosSeleccionados) {
-    return <h1>{i18n.AWARDS.NOT_FOUND}</h1>
-  }
+    function selectPalmares(idx: number) {
+      index.value = idx
+    }
 
-  const {
-    comunidad1,
-    comunidad2,
-    finalista,
-    foto1,
-    foto2,
-    ganador,
-    streamers1,
-    streamers2,
-    total1,
-    total2,
-  } = datosSeleccionados?.info[index]
-
-  return (
-    <section class="max-w-6xl mx-auto flex flex-col gap-4 pt-20 justify-center items-center px-20">
-      <h2 class="text-4xl lg:text-6xl text-center mb-10 lg:mb-20 uppercase font-tomaso">
-        {i18n.AWARDS.TITLE}
-      </h2>
-      <div class="flex flex-col lg:flex-row gap-10">
-        <div class="flex flex-col gap-2">
-          {palmares.map((item) => {
-            if (item.edition === edicion) {
-              return item.info.map((infoItem, i) => {
-                const { categoria, ganador } = infoItem
-                return (
-                  <button
-                    id={`${i + 1}`}
-                    class={`flex justify-between items-center rounded-sm ${
-                      styles.button
-                    } ${
-                      i % 2 === 0
-                        ? 'bg-gradient-to-r from-blue-600 to-sky-400/45  '
-                        : 'bg-gradient-to-r from-gray-700 to-blue-900'
-                    } ${
-                      index === i
-                        ? 'bg-blue-950 border border-sky-300 border-spacing-6'
-                        : ''
-                    } text-center hover:bg-blue-900 px-4 py-1`}
-                  >
-                    <h1 class="text-sm text-left">{categoria}</h1>
-                    <strong class="text-xs text-right">{ganador}</strong>
-                  </button>
-                )
-              })
-            }
-          })}
-        </div>
-        <div class="flex gap-3 items-end mr-5">
-          {ganador && (
-            <StreamerCard
-              position="1º"
-              edicion={edicion}
-              name={ganador}
-              comunidad={comunidad1 ?? 0}
-              streamers={streamers1 ?? 0}
-              foto={foto1}
-              total={total1 ?? 0}
-              background={'bg-finalista'}
-            />
+    return () => (
+      <section class="max-w-6xl mx-auto flex flex-col gap-4 pt-20 justify-center items-center px-20">
+        <h2 class="text-4xl lg:text-6xl text-center mb-10 lg:mb-20 uppercase font-tomaso">
+          {props.i18n.AWARDS.TITLE}
+        </h2>
+        <div class="flex flex-col lg:flex-row gap-10">
+          <div class="flex flex-col gap-2">
+            {palmares.map((item) => {
+              if (item.edition === props.edicion) {
+                return item.info.map((infoItem, i) => {
+                  const { categoria, ganador } = infoItem
+                  return (
+                    <button
+                      id={`${i + 1}`}
+                      class={`flex justify-between items-center rounded-sm ${
+                        styles.button
+                      } ${
+                        i % 2 === 0
+                          ? 'bg-gradient-to-r from-blue-600 to-sky-400/45  '
+                          : 'bg-gradient-to-r from-gray-700 to-blue-900'
+                      } ${
+                        index.value === i
+                          ? 'bg-blue-950 border border-sky-300 border-spacing-6'
+                          : ''
+                      } text-center hover:bg-blue-900 px-4 py-1`}
+                      onClick={() => selectPalmares(i)}
+                    >
+                      <h1 class="text-sm text-left">{categoria}</h1>
+                      <strong class="text-xs text-right">{ganador}</strong>
+                    </button>
+                  )
+                })
+              }
+            })}
+          </div>
+          {palmar.value && (
+            <div class="flex gap-3 items-end mr-5">
+              {palmar.value.ganador && (
+                <StreamerCard
+                  position="1º"
+                  edicion={props.edicion}
+                  name={palmar.value.ganador}
+                  comunidad={palmar.value.comunidad1 ?? 0}
+                  streamers={palmar.value.streamers1 ?? 0}
+                  foto={palmar.value.foto1}
+                  total={palmar.value.total1 ?? 0}
+                  background={'bg-finalista'}
+                />
+              )}
+              {palmar.value.finalista && (
+                <StreamerCard
+                  position="2º"
+                  edicion={props.edicion}
+                  name={palmar.value.finalista}
+                  comunidad={palmar.value.comunidad2}
+                  streamers={palmar.value.streamers2}
+                  foto={palmar.value.foto2}
+                  total={palmar.value.total2}
+                  background={'bg-semifinalista'}
+                />
+              )}
+            </div>
           )}
-          {finalista && (
-            <StreamerCard
-              position="2º"
-              edicion={edicion}
-              name={finalista}
-              comunidad={comunidad2}
-              streamers={streamers2}
-              foto={foto2}
-              total={total2}
-              background={'bg-semifinalista'}
-            />
-          )}
         </div>
-      </div>
-    </section>
-  )
-}
+      </section>
+    )
+  },
+})
