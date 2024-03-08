fce479437cce843776c8b0cc170f03787471c2ab
memoTux <romeo@mendezfuentes.net>
2024-03-08 17:34:13 -0600

    [config] add astro integration vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import preact from "@astrojs/preact";
-
-import auth from "auth-astro";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import preact from '@astrojs/preact'
+import vue from '@astrojs/vue'
+import auth from 'auth-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), auth()],
+  integrations: [tailwind(), preact(), vue(), auth()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'ca'],
     routing: {
-      prefixDefaultLocale: false
-    }
+      prefixDefaultLocale: false,
+    },
   },
-  output: "server",
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel(),
+})
